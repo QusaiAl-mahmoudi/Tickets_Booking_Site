d5abc9fdcca6891d8e521f4e0ad1306d013744a4
Qusai Adam Abdullah <almhmoudiqusai@gmail.com>
2025-05-28 01:40:58 +0300

    Create main.java

---
+++ src/main.java
@@ -0,0 +1,20 @@
+/*
+ *  >> Al mhmoidi .~
+ *  creat by/ Qusai adam >>
+ */
+
+package booking_tickets;
+
+public class Main extends Booking_Tickets1{
+public static void main(String[] args) throws IOException, InterruptedException {  
+  Login(); 
+  //clear console    
+  clear();     
+  //Project execution (project start).     
+  userInterface();   
+  //message of exit of program   
+   println("Thank you for use my program .Goodbye!");
+    }//End of Methode Main
+  
+}
+
